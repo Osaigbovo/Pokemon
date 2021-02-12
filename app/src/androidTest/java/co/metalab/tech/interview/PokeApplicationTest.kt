ef25d4c4b7682e331b9ea7d410ce3deefe268d3e
Osaigbovo Odiase <odiase.osaigbovo@gmail.com>
2021-02-13 00:02:56 +0100

    update dependencies

--- app/src/androidTest/java/co/metalab/tech/interview/PokeApplicationTest.kt
+++ app/src/androidTest/java/co/metalab/tech/interview/PokeApplicationTest.kt
@@ -1,7 +1,10 @@
 package co.metalab.tech.interview
 
-import co.metalab.tech.interview.di.AppComponent
-
+/**
+ * We use a separate App for tests to prevent initializing dependency injection.
+ *
+ * See [co.metalab.tech.interview.util.PokeTestRunner].
+ */
 class PokeApplicationTest : PokeApplication() {
 
 /*    override fun initializeComponent(): AppComponent {

e93d0b7e45e8855898d90f6955544517b3adb288
Osaigbovo Odiase <odiase.osaigbovo@gmail.com>
2021-02-12 23:47:42 +0100

    update dependencies

--- app/src/androidTest/java/co/metalab/tech/interview/PokeApplicationTest.kt
+++ app/src/androidTest/java/co/metalab/tech/interview/PokeApplicationTest.kt
@@ -4,9 +4,9 @@ import co.metalab.tech.interview.di.AppComponent
 
 class PokeApplicationTest : PokeApplication() {
 
-    override fun initializeComponent(): AppComponent {
+/*    override fun initializeComponent(): AppComponent {
         return DaggerTestAppComponent
             .factory()
             .create(this)
-    }
+    }*/
 }
